655943f4fc0128eb14b506724a3da71645a4e7dd
alfian0 <alfian.official.mail@gmail.com>
2024-09-30 15:13:46 +0700

    Initial Commit

---
+++ TextValidator/TextValidatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextValidatorApp.swift
+//  TextValidator
+//
+//  Created by Alfian on 30/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TextValidatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
